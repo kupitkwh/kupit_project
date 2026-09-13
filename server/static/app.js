30d1c689eab4340dfaa9cb4166f54fbf935bef56
Hidayat Tullah <ah3577470@gmail.com>
2026-09-13 20:03:05 +0700

    Update project

--- server/static/app.js
+++ server/static/app.js
@@ -1,5 +1,5 @@
 /* ── CONFIG ──────────────────────────────────────────────── */
-let BASE_URL = 'https://web-production-a4abd.up.railway.app/';
+let BASE_URL = 'https://web-production-0f9c26.up.railway.app/';
 let POLL_INTERVAL = 5000;
 let isTicking = false;
 
